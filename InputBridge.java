4af66a81d3392f19ea74335a921a266cea04cb68
Tristan Adams <98128375+TristanA1016@users.noreply.github.com>
2024-11-22 22:57:46 -0600

    Delete InputBridge.java

--- InputBridge.java
+++
@@ -1,6 +0,0 @@
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
-public class InputBridge {
-    public static final BlockingQueue<Integer> inputQueue = new LinkedBlockingQueue<>();
-}
