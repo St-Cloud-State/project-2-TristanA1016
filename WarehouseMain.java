742c98c107fd68cbd1fc66f2578db3badbf0a5a0
Tristan Adams <98128375+TristanA1016@users.noreply.github.com>
2024-11-22 22:58:48 -0600

    Delete WarehouseMain.java

--- WarehouseMain.java
+++
@@ -1,9 +0,0 @@
-public class WarehouseMain {
-    public static void main(String[] args) {
-        WarehouseContext.instance().start();
-        WarehouseManagementSystem wms = new WarehouseManagementSystem();
-        wms.setSystemID(1);
-        wms.run();
-        
-    }
-}
